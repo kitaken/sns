532cc956effe982576db23669c25e424ed711bd4
ymrl <ymrl@basquiat.(none)>
2011-12-18 01:13:36 +0900

    add type column to articles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111214115141) do
+ActiveRecord::Schema.define(:version => 20111217161114) do
 
   create_table "answers", :force => true do |t|
     t.integer  "question_id"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(:version => 20111214115141) do
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "type"
   end
 
   create_table "events", :force => true do |t|
