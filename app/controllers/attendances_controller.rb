de26a0977b3216756b14c6a22e1a35b5fc9aae8e
ymrl <ymrl@ymrl.net>
2011-12-26 15:27:22 +0900

    attendance

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,14 +1,12 @@
 class AttendancesController < ApplicationController
   before_filter :authenticate_user!
-  def new
-    create
-  end
+
   # POST /attendances
   # POST /attendances.json
   def create
-    @attendance = Attendance.find(:first,:user_id=>current_user.id,:event_id=>params[:event_id]) || Attendance.new(params[:attendance])
-    @attendance.user_id => current_user.id
-    @attendance.event_id=> params[:event_id]
+    @attendance = Attendance.find(:first,:conditions=>{:user_id=>current_user.id,:event_id=>params[:event_id]}) || Attendance.new(params[:attendance])
+    @attendance.user_id  = current_user.id
+    @attendance.event_id = params[:event_id]
 
     respond_to do |format|
       if @attendance.save
@@ -29,10 +27,10 @@ class AttendancesController < ApplicationController
 
     respond_to do |format|
       if @attendance.update_attributes(params[:attendance])
-        format.html { redirect_to @attendance, notice: 'Attendance was successfully updated.' }
+        format.html { redirect_to event_path(@attendance.event_id), notice: 'Attendance was successfully updated.' }
         format.json { head :ok }
       else
-        format.html { render action: "edit" }
+        format.html { redirect_to event_path(@attendance.event_id), notice: 'Something is wrong while updating attendance.' }
         format.json { render json: @attendance.errors, status: :unprocessable_entity }
       end
     end
--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,4 +1,4 @@
 class Attendance < ActiveRecord::Base
-  has_one :user
+  belongs_to :user
   belongs_to :event
 end
