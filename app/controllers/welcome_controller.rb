9bcb2a82a388deaa64c6e3162182515cb46efb95
ymrl <ymrl@ymrl.net>
2012-02-14 13:54:47 +0900

    activities can be seen  where welcome#index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,8 @@
 class WelcomeController < ApplicationController
   before_filter :authenticate_user!
   def index
+    friend_ids = current_user.friends.where('friendships.status="friend"').select('users.id').map(&:id)
+    @activities = Activity.where('user_id in (?) or category_id = ?',friend_ids.join(','),current_user.category_id)
   end
 
   def preferences
--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,2 +1,5 @@
 class Activity < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :category
+  belongs_to :model, :polymorphic => true
 end
